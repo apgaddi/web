acb475b3705bb77ba6e62507e73f8b11b25cd9ba
apgaddi <65837536+apgaddi@users.noreply.github.com>
2023-09-01 04:44:39 +0800

    Add files via upload

---
+++ App_Code/DBMaster.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApp.App_Code
+{
+    public class DBMaster
+    {
+        private SqlConnection con;
+
+        public SqlConnection getConnection()
+        {
+           
+            //connect to db
+              
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["connect"].ToString());
+                con.Open();
+                return con;
+        }
+        public SqlDataReader getReader(string query)
+        {
+            //create command
+
+                SqlCommand cmd = new SqlCommand(query);
+                cmd.CommandType=System.Data.CommandType.Text;
+                cmd.Connection = this.getConnection();
+
+       
+                //read from db
+
+                SqlDataReader rdr = cmd.ExecuteReader();
+                return rdr;
+        }
+        
+        
+        public void closeConnection()
+        {
+            if (con!=null && con.State==System.Data.ConnectionState.Open)
+            {
+                this.con.Close();
+            }
+        }
+    }
+}
